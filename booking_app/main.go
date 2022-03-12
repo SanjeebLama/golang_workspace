05892e6a87dac4fd7b5acc5437918f8106693703
Sanjeeb <sanjeeb.lama004@gmail.com>
2022-03-12 19:21:48 +0545

    Booking App

---
+++ booking_app/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var first_name string
+	// var last_name string
+
+	// var email string
+	// var ticket uint
+
+	// const total_ticket = 50
+
+	// fmt.Println("Welcome to Booking App")
+	// fmt.Println("Enter your First name:")
+	// fmt.Scan(&first_name)
+	// fmt.Println("Enter your Last name:")
+	// fmt.Scan(&last_name)
+	// fmt.Println("Enter your email:")
+	// fmt.Scan(&email)
+	// fmt.Println("How any ticket do you want to book?")
+	// fmt.Scan(&ticket)
+
+	// fmt.Printf("Dear %s%s, you have booked %d tickets. Your ticket confirmation is sent to your email: %s \n Thank you\n", first_name, last_name, ticket, email)
+
+	// fmt.Println("Remaining Ticket :", total_ticket-ticket)
+
+	// fmt.Println(`"Hello ", checking `, first_name)
+
+	test := "onetwothree"
+	fmt.Println(len(test))
+}
