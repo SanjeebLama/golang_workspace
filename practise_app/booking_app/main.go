f1bf98d4a4c037eb6bdfffa8ad653ec872cffe41
Sanjeeb Lama <51410633+SanjeebLama@users.noreply.github.com>
2023-05-24 12:00:40 +0545

    Fix: Sonar Lint Error - Rename this local variable to match the regular expression "^(_|[a-zA-Z0-9]+)$".sonarlint(go:S117)

--- practise_app/booking_app/main.go
+++ practise_app/booking_app/main.go
@@ -3,30 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	// var first_name string
-	// var last_name string
+	var firstName string
+	var lastName string
 
-	// var email string
-	// var ticket uint
+	var email string
+	var ticket uint
 
-	// const total_ticket = 50
+	const totalTicket = 50
 
-	// fmt.Println("Welcome to Booking App")
-	// fmt.Println("Enter your First name:")
-	// fmt.Scan(&first_name)
-	// fmt.Println("Enter your Last name:")
-	// fmt.Scan(&last_name)
-	// fmt.Println("Enter your email:")
-	// fmt.Scan(&email)
-	// fmt.Println("How any ticket do you want to book?")
-	// fmt.Scan(&ticket)
+	fmt.Println("Welcome to Booking App")
+	fmt.Println("Enter your First name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your Last name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email:")
+	fmt.Scan(&email)
+	fmt.Println("How any ticket do you want to book?")
+	fmt.Scan(&ticket)
 
-	// fmt.Printf("Dear %s%s, you have booked %d tickets. Your ticket confirmation is sent to your email: %s \n Thank you\n", first_name, last_name, ticket, email)
+	fmt.Printf("Dear %s%s, you have booked %d tickets. Your ticket confirmation is sent to your email: %s \n Thank you\n", firstName, lastName, ticket, email)
 
-	// fmt.Println("Remaining Ticket :", total_ticket-ticket)
+	fmt.Println("Remaining Ticket :", totalTicket-ticket)
 
-	// fmt.Println(`"Hello ", checking `, first_name)
+	// fmt.Println(`"Hello ", checking `, firstName)
 
-	test := "onetwothree"
-	fmt.Println(len(test))
+	// test := "onetwothree"
+	// fmt.Println(len(test))
 }
