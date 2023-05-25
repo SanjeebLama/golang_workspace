f786f8a37134e62a08ee3c152957e4af7bfeb7ed
Sanjeeb Lama <51410633+SanjeebLama@users.noreply.github.com>
2023-05-25 15:48:51 +0545

    Multiple Return Type

--- Cards/deck.go
+++ Cards/deck.go
@@ -13,10 +13,31 @@ type deck []string
 // 	}
 // }
 
-func (d deck) printCards() {
+func (d deck) displayCards() {
+
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
 // we are using d  as receiver function because this is a deck type and it's convention to define it with single letter.
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds"}
+
+	cardValues := []string{"Ace", "Two", "Three"}
+
+	for _, suit := range cardSuits {
+		for _, values := range cardValues {
+			cards = append(cards, values+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
--- Cards/state.go
+++ Cards/state.go
@@ -1,22 +1,35 @@
 package main
 
-func printCard() {
-	cards := getCard()
+import "fmt"
 
-	newList := append(cards, "Six of Spades")
+func printCard() {
+	cards := newDeck()
+
+	// newList := append(cards, "Six of Spades")
 
 	// for i, card := range newList { // this will throw error as we are not using index
 	// 	fmt.Println(card)
 	// }
 
-	newList.printCards() // We are passing newList to printCards() printCards is inside deck.go
+	// newList.displayCards() // We are passing newList to printCards() printCards is inside deck.go
+
+	hand, remainingCards := deal(cards, 5)
+
+	// fmt.Println("Hand : ", hand, "\nRemaining Cards : ", remainingCards)
+
+	fmt.Println("Hand : ")
+	hand.displayCards()
+
+	fmt.Println("Remaining Cards : ")
+	remainingCards.displayCards()
+
 }
 
-func getCard() deck { // deck is a custom type
-	cardList := deck{"Ace of Diamonds", newCard()}
-	return cardList
-}
+// func getCard() deck { // deck is a custom type
+// 	cardList := deck{"Ace of Diamonds", newCard()}
+// 	return cardList
+// }
 
-func newCard() string {
-	return "Ace of Spades"
-}
+// func newCard() string {
+// 	return "Ace of Spades"
+// }
