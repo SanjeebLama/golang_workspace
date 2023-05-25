5388d78d4d6a37e7662a221db8f877d23f68f7ff
Sanjeeb Lama <51410633+SanjeebLama@users.noreply.github.com>
2023-05-25 13:21:18 +0545

    Variables

---
+++ Cards/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var deckSize int
+
+func main() {
+	fmt.Println("Hello From Cards")
+
+	card := "Ace of Spades"
+	fmt.Println(card)
+	card = "King of Diamonds"
+	fmt.Println(card)
+
+	deckSize = 20
+
+	fmt.Println(deckSize)
+
+}
