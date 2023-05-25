5fccaa74a8668f1e11c4658fb95ce35f3e344fd4
Sanjeeb Lama <51410633+SanjeebLama@users.noreply.github.com>
2023-05-25 13:47:50 +0545

    For Range Loop

--- Cards/state.go
+++ Cards/state.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func printCard() {
-	card := getCard()
-	fmt.Println(card)
+	cards := getCard()
+
+	newList := append(cards, "Six of Spades")
+
+	for i, card := range newList { // this is for range loop
+		fmt.Println(i, card)
+	}
 }
 
-func getCard() string {
-	return "Five of Diamonds"
+func getCard() []string {
+	cardList := []string{"Ace of Diamonds", newCard()}
+	return cardList
+}
+
+func newCard() string {
+	return "Ace of Spades"
 }
