9990221cd30b9acab0fb4a2d78b6045c6e1fe313
Sanjeeb Lama <51410633+SanjeebLama@users.noreply.github.com>
2023-05-25 13:31:52 +0545

    Function and return type You need to run both main and state file

--- Cards/main.go
+++ Cards/main.go
@@ -1,19 +1,5 @@
 package main
 
-import "fmt"
-
-var deckSize int
-
 func main() {
-	fmt.Println("Hello From Cards")
-
-	card := "Ace of Spades"
-	fmt.Println(card)
-	card = "King of Diamonds"
-	fmt.Println(card)
-
-	deckSize = 20
-
-	fmt.Println(deckSize)
-
+	printCard()
 }
