c29682f8d777c682b5f9de28e7be0e43700a1c16
Sanjeeb Lama <51410633+SanjeebLama@users.noreply.github.com>
2023-05-27 10:12:32 +0545

    Pointer in Sturct : Golang is pass by Value

--- structs/main.go
+++ structs/main.go
@@ -5,6 +5,13 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	// contact   contactInfo
+	contactInfo
+}
+
+type contactInfo struct {
+	email string
+	zip   int
 }
 
 func main() {
@@ -13,25 +20,48 @@ func main() {
 	// tony := person{"Tony", "Stark"} // donot use it like this
 
 	// tony := person{firstName: "Tony", lastName: "Stark"}
-
+	//
 	// fmt.Println(tony)
 
 	// fmt.Println(tony.firstName)
 	// fmt.Println(tony.lastName)
 	// fmt.Printf("%T", tony.firstName)
 
-	var ironMan person
+	// var ironMan person
 
-	fmt.Println(ironMan)
+	// fmt.Println(ironMan)
 
-	fmt.Println(ironMan.firstName)
+	// fmt.Println(ironMan.firstName)
 
-	fmt.Println(ironMan.lastName)
+	// fmt.Println(ironMan.lastName)
 
-	fmt.Printf("%+v", ironMan)
+	// fmt.Printf("%+v", ironMan)
 
-	ironMan.firstName = "Tony"
-	ironMan.lastName = "Stark"
+	// ironMan.firstName = "Tony"
+	// ironMan.lastName = "Stark"
+	// ironMan.contact.email = "tony@stark.industries"
 
-	fmt.Println(ironMan)
+	// fmt.Println(ironMan)
+
+	var tony = person{
+		lastName:  "Stark",
+		firstName: "Tony",
+		contactInfo: contactInfo{
+			zip:   123,
+			email: "tony@stark.com",
+		},
+	}
+
+	tonyPointer := &tony
+	tonyPointer.updateFirstName("IronMan") // This won't work because we are not using pointer
+
+	tony.print()
+}
+
+func (p person) print() {
+	fmt.Printf(" %+v", p)
+}
+
+func (pointerToPersonValue *person) updateFirstName(newFirstName string) {
+	(*pointerToPersonValue).firstName = newFirstName
 }
