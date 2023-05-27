b76a7e66331380a3e3051f81679e7884e096f38d
Sanjeeb Lama <51410633+SanjeebLama@users.noreply.github.com>
2023-05-27 21:49:41 +0545

    Add different method to read and Custom Writer

--- go-basics/interface - bot/http/main.go
+++ go-basics/interface - bot/http/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	fmt.Println("Hello HTTP")
@@ -13,10 +17,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	// fmt.Println("Response : ", res)
 
-	bs := make([]byte, 99999)
-	res.Body.Read(bs)
+	/*******
+	** Method 1: Read Response body
+	**/
 
+	// bs := make([]byte, 99999) // This will allocate memory for the response
+	// fmt.Println(len(bs))      // returns 99999
+
+	// fmt.Printf("Type of bs :  %T\n", bs) // returns []uint8
+
+	// res.Body.Read(bs)
+
+	// fmt.Println(string(bs))
+
+	/*******
+	** Method 2: Read Response body
+	**/
+
+	io.Copy(os.Stdout, res.Body)
+
+	/*******
+	** Creating custom Writer
+	**/
+
+	lw := logWriter{}
+	io.Copy(lw, res.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes : ", len(bs))
+	return len(bs), nil
 }
